ece34226cfcf0aa42cd25f6d4acda4385aff1b06
Jatin soni <66208658+Jkverma1@users.noreply.github.com>
2022-02-09 18:18:55 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,15 +2,49 @@ function clearScreen() {
     document.getElementById("result").value = "";
    }
 
-   function display(value) {
+function display(value) {
     document.getElementById("result").value += value;
-   }
+}
 
-   function calculate() {
+function status(){
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    q = q*(-1)
+    document.getElementById("result").value = q;
+}
+
+function remove(){
+    var bsp = document.getElementById("result").value;
+    document.getElementById("result").value=bsp.substring(0,bsp.length -1);
+}
+
+function mode(){
+    if(value == 0){
+    document.getElementById("mode").value = "Night";
+    var calculator = document.querySelector(".calculator");
+    calculator.style.backgroundColor = "#383838";
+    var displayBox = document.querySelector(".display-box");
+    displayBox.style.backgroundColor = "#1f1f1f";
+    displayBox.style.color = "white";
+    value = 1;
+    }
+    else 
+    {   document.getElementById("mode").value = "Day";
+        var calculator = document.querySelector(".calculator");
+        calculator.style.backgroundColor = "white";
+        var displayBox = document.querySelector(".display-box");
+        displayBox.style.backgroundColor = "white";
+        displayBox.style.color = "black";
+        value = 0;
+    }
+}
+
+
+function calculate() {
     var p = document.getElementById("result").value;
     var q = eval(p);
     document.getElementById("result").value = q;
-   }
+}
 
 
 
